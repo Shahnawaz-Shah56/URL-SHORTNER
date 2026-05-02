f57af729639397c094da751ebf6333847b5ab2e6
Shahnawaz Shah <shahcreation6158@gmail.com>
2026-05-02 13:08:57 +0530

    ssl and tls fixed

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 async function connectDB(){
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URI,{ssl: true,
+    tls: true})
     console.log('MongoDB connected ✅')
 }
 
